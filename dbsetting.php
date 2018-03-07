699096efefc7bf0200ca64cec9ce408d1d2c35bb
temp_sramapuram <temp_sramapuram@INFRAMAPURAMSMAC.local>
2018-03-07 14:17:48 -0500

    adding files

---
+++ dbsetting.php
@@ -0,0 +1,17 @@
+<?php
+
+#####################################
+## START: MYSQL CONNECTION SETINGS
+#####################################
+$hostname = 'localhost';
+$database = 'rel';
+$username = 'reluser';
+$password = 'Wx#$dhdskljfsdfljpo3487';
+## NEW mysqli API
+$mysqli = mysqli_connect($hostname, $username, $password, $database);
+#####################################
+## END: MYSQL CONNECTION SETINGS
+#####################################
+
+?>
+
